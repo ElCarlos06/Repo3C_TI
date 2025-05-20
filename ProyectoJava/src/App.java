952629a84c4a478966dce34f13bd8fe5738df09a
ElCarlos06 <20243ds085@utez.edu.mx>
2025-05-20 09:30:39 -0600

    Se termina el archivo java

--- ProyectoJava/src/App.java
+++ ProyectoJava/src/App.java
@@ -2,14 +2,17 @@ import javax.swing.JOptionPane;
 
 public class App {
     public static void main(String[] args) {
-        int sum;
-        float res;
+        int sum, res;
 
         String num1 = JOptionPane.showInputDialog("Ingresa numero 1: ");
         String num2 = JOptionPane.showInputDialog("Ingresa numero 2: ");
         String num3 = JOptionPane.showInputDialog("Ingresa numero 3: ");
         
+        sum = Integer.parseInt(num1)+Integer.parseInt(num2)+Integer.parseInt(num3);
+        res = sum/3;
 
+        System.out.println(sum);
+        System.out.println(res);
 
     }
     
