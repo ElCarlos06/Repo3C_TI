f2be94ac5d5b42caab0f8e466159e1626f8ce7ab
ElCarlos06 <20243ds085@utez.edu.mx>
2025-05-20 09:27:49 -0600

    Se crea el proyecto en el repositorio

---
+++ ProyectoJava/src/App.java
@@ -0,0 +1,16 @@
+import javax.swing.JOptionPane;
+
+public class App {
+    public static void main(String[] args) {
+        int sum;
+        float res;
+
+        String num1 = JOptionPane.showInputDialog("Ingresa numero 1: ");
+        String num2 = JOptionPane.showInputDialog("Ingresa numero 2: ");
+        String num3 = JOptionPane.showInputDialog("Ingresa numero 3: ");
+        
+
+
+    }
+    
+}
