f36aa8d6eb19be770560bd7a13f0e73267d41fb4
ElCarlos06 <20243ds085@utez.edu.mx>
2025-05-22 12:15:53 -0600

    Nomenclaturas

---
+++ U1_TI_02_AreasPerimetros/src/App.java
@@ -0,0 +1,32 @@
+import Model.Circle;
+import Model.Square;
+import Model.Triangle;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        Square sq = new Square();
+        sq.setLado(10);
+        sq.setMessage("");
+        System.out.println("Lado del cuadrado: " + sq.getLado());
+        double squarePerimeter = sq.getPerimeter();
+        System.out.println("Perimetro de cuadrado: " + squarePerimeter);
+        double squareArea = sq.getArea();
+        System.out.println("area dek cuadrado: " + squareArea);
+        System.out.println(sq.getArea());
+        
+        Circle cr = new Circle();
+        cr.setRadio(10);
+        System.out.println("Radio del circulo: " + cr.getRadio());
+        System.out.println("Perimetro del circulo: " + cr.getPerimeter());
+        System.out.println("Area del circulo: " + cr.getArea());
+        
+        Triangle tr = new Triangle();
+        tr.setAltura(10);
+        tr.setBase(10);
+        tr.setLado(11);
+        System.out.println("Lado del traingulo: " + tr.getLado());
+        System.out.println("Area del traingulo: " + tr.getArea());
+        System.out.println("Perimetro del traingulo: " + tr.getPerimeter());
+    }
+}
