2a8c69018a063910190ecd012b2cb95c2572ebd6
ElCarlos06 <20243ds085@utez.edu.mx>
2025-05-27 09:15:06 -0600

    Agregar titular en actividad 5

--- U1_TI_05_CuentaBancaria/src/App.java
+++ U1_TI_05_CuentaBancaria/src/App.java
@@ -3,7 +3,10 @@ import Model.CuentaBancaria;
 public class App {
     public static void main(String[] args) throws Exception {
         CuentaBancaria accountBank = new CuentaBancaria();
+        
+        accountBank.setTitular("Andres Manuel");
 
+        System.out.println("TItular: "+ accountBank.getTitular());
         accountBank.depositar(1000);
         accountBank.getSaldo();
 
